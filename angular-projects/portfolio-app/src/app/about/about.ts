c0e28e272a64c525162d73044400c34dbdf89860
Aryan Tiwari <aryantiwari522@gmail.com>
2026-04-26 18:32:49 +0530

    AR-2 refractor few changes related to contact and icons

--- angular-projects/portfolio-app/src/app/about/about.ts
+++ angular-projects/portfolio-app/src/app/about/about.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './about.html',
   styleUrls: ['./about.css'],
 })
-export class About {}
+export class About {
+  expanded: { [key: string]: boolean } = {};
+
+  toggleExpand(key: string) {
+    this.expanded[key] = !this.expanded[key];
+  }
+}
