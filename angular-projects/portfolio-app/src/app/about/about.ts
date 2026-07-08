97fdf90987f77ba4041fcea535588c27baa268c0
Aryan Tiwari <aryantiwari522@gmail.com>
2026-07-08 16:50:31 +0530

    updated info

--- angular-projects/portfolio-app/src/app/about/about.ts
+++ angular-projects/portfolio-app/src/app/about/about.ts
@@ -12,6 +12,13 @@ export class About {
   expanded: { [key: string]: boolean } = {};
 
   toggleExpand(key: string) {
+    // Close all other items
+    Object.keys(this.expanded).forEach(k => {
+      if (k !== key) {
+        this.expanded[k] = false;
+      }
+    });
+    // Toggle the clicked item
     this.expanded[key] = !this.expanded[key];
   }
 }
